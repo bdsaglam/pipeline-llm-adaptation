d7d10270136e51c75abf89a00e501d83b4278486
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-02-15 10:33:08 +0300

    first set of experiments

--- nbs/erx/run.sh
+++ nbs/erx/run.sh
@@ -1,5 +1,34 @@
 #!/bin/sh
 
+dvc exp run --queue \
+    -S run=1 \
+    -S train.optimizer='miprov2-light' \
+    -S train.ensemble='no' \
+    -S lm.model='qwen-2.5-32b' \
+    -S lm.temperature='0.0' \
+    -S run='1' \
+    -S task='erx' \
+    -S train.dataset.path='bdsaglam/web_nlg-erx-concat' \
+    -S train.dataset.name='release_v3.0_en' \
+    -S train.dataset.split='"train[:100]"' \
+    -S evaluation.dataset.path='bdsaglam/web_nlg-erx-concat' \
+    -S evaluation.dataset.name='release_v3.0_en' \
+    -S evaluation.dataset.split='"dev[:100]"'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S train.optimizer='bfsrs-medium' \
+    -S train.ensemble='no' \
+    -S lm.model='qwen-2.5-32b' \
+    -S lm.temperature='0.0' \
+    -S run='1' \
+    -S task='erx' \
+    -S train.dataset.path='bdsaglam/web_nlg-erx-concat' \
+    -S train.dataset.name='release_v3.0_en' \
+    -S train.dataset.split='"train[:100]"' \
+    -S evaluation.dataset.path='bdsaglam/web_nlg-erx-concat' \
+    -S evaluation.dataset.name='release_v3.0_en' \
+    -S evaluation.dataset.split='"dev[:100]"'
 
 dvc exp run --queue \
     -S run=1 \
@@ -15,64 +44,3 @@ dvc exp run --queue \
     -S evaluation.dataset.path='bdsaglam/web_nlg-erx-concat' \
     -S evaluation.dataset.name='release_v3.0_en' \
     -S evaluation.dataset.split='"dev[:100]"'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.optimizer='bfsrs-medium' \
-    -S train.ensemble='no' \
-    -S lm.model='llama-3-8b' \
-    -S lm.temperature='0.0' \
-    -S run='1' \
-    -S task='erx' \
-    -S train.dataset.path='bdsaglam/web_nlg-erx-concat' \
-    -S train.dataset.name='release_v3.0_en' \
-    -S train.dataset.split='"train[:100]"' \
-    -S evaluation.dataset.path='bdsaglam/web_nlg-erx-concat' \
-    -S evaluation.dataset.name='release_v3.0_en' \
-    -S evaluation.dataset.split='"dev[:100]"'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.optimizer='miprov2-light' \
-    -S train.ensemble='no' \
-    -S lm.model='qwen-2.5-32b' \
-    -S lm.temperature='0.0' \
-    -S run='1' \
-    -S task='erx' \
-    -S train.dataset.path='bdsaglam/web_nlg-erx-concat' \
-    -S train.dataset.name='release_v3.0_en' \
-    -S train.dataset.split='"train[:100]"' \
-    -S evaluation.dataset.path='bdsaglam/web_nlg-erx-concat' \
-    -S evaluation.dataset.name='release_v3.0_en' \
-    -S evaluation.dataset.split='"dev[:100]"'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.optimizer='miprov2-light' \
-    -S train.ensemble='no' \
-    -S lm.model='llama-3-8b' \
-    -S lm.temperature='0.0' \
-    -S run='1' \
-    -S task='erx' \
-    -S train.dataset.path='bdsaglam/web_nlg-erx-concat' \
-    -S train.dataset.name='release_v3.0_en' \
-    -S train.dataset.split='"train[:100]"' \
-    -S evaluation.dataset.path='bdsaglam/web_nlg-erx-concat' \
-    -S evaluation.dataset.name='release_v3.0_en' \
-    -S evaluation.dataset.split='"dev[:100]"'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S train.optimizer='bfsrs-medium' \
-    -S train.ensemble='no' \
-    -S lm.model='qwen-2.5-32b' \
-    -S lm.temperature='0.0' \
-    -S run='1' \
-    -S task='erx' \
-    -S train.dataset.path='bdsaglam/web_nlg-erx-concat' \
-    -S train.dataset.name='release_v3.0_en' \
-    -S train.dataset.split='"train[:100]"' \
-    -S evaluation.dataset.path='bdsaglam/web_nlg-erx-concat' \
-    -S evaluation.dataset.name='release_v3.0_en' \
-    -S evaluation.dataset.split='"dev[:100]"'
-
