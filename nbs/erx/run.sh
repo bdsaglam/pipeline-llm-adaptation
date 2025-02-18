1a59e5b773a7cdf52fe585bf83a8d883be8447e0
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-02-18 22:02:37 +0300

    add new leaderboard

--- nbs/erx/run.sh
+++ nbs/erx/run.sh
@@ -11,24 +11,9 @@ dvc exp run --queue \
     -S train.ensemble='no' \
     -S lm.temperature='0.0' \
     -S run='1' \
-    -S train.optimizer='bfsrs-ulti' \
-    -S program.prompting='structured' \
-    -S lm.model='llama-3-8b'
-
-dvc exp run --queue \
-    -S task='erx' \
-    -S train.dataset.path='bdsaglam/web_nlg-erx-concat' \
-    -S train.dataset.name='release_v3.0_en' \
-    -S train.dataset.split='"train[:100]"' \
-    -S evaluation.dataset.path='bdsaglam/web_nlg-erx-concat' \
-    -S evaluation.dataset.name='release_v3.0_en' \
-    -S evaluation.dataset.split='"dev"' \
-    -S train.ensemble='no' \
-    -S lm.temperature='0.0' \
-    -S run='1' \
     -S train.optimizer='bfsrs-high' \
     -S program.prompting='structured' \
-    -S lm.model='llama-3-8b'
+    -S lm.model='llama-3.3-70b'
 
 dvc exp run --queue \
     -S task='erx' \
@@ -41,39 +26,9 @@ dvc exp run --queue \
     -S train.ensemble='no' \
     -S lm.temperature='0.0' \
     -S run='1' \
-    -S train.optimizer='noop' \
+    -S train.optimizer='bfsrs-ulti' \
     -S program.prompting='structured' \
-    -S lm.model='llama-3-8b'
-
-dvc exp run --queue \
-    -S task='erx' \
-    -S train.dataset.path='bdsaglam/web_nlg-erx-concat' \
-    -S train.dataset.name='release_v3.0_en' \
-    -S train.dataset.split='"train[:100]"' \
-    -S evaluation.dataset.path='bdsaglam/web_nlg-erx-concat' \
-    -S evaluation.dataset.name='release_v3.0_en' \
-    -S evaluation.dataset.split='"dev"' \
-    -S train.ensemble='no' \
-    -S lm.temperature='0.0' \
-    -S run='1' \
-    -S train.optimizer='miprov2-light' \
-    -S program.prompting='structured' \
-    -S lm.model='llama-3-8b'
-
-dvc exp run --queue \
-    -S task='erx' \
-    -S train.dataset.path='bdsaglam/web_nlg-erx-concat' \
-    -S train.dataset.name='release_v3.0_en' \
-    -S train.dataset.split='"train[:100]"' \
-    -S evaluation.dataset.path='bdsaglam/web_nlg-erx-concat' \
-    -S evaluation.dataset.name='release_v3.0_en' \
-    -S evaluation.dataset.split='"dev"' \
-    -S train.ensemble='no' \
-    -S lm.temperature='0.0' \
-    -S run='1' \
-    -S train.optimizer='miprov2-medium' \
-    -S program.prompting='structured' \
-    -S lm.model='llama-3-8b'
+    -S lm.model='llama-3.3-70b'
 
 dvc exp run --queue \
     -S task='erx' \
@@ -88,5 +43,50 @@ dvc exp run --queue \
     -S run='1' \
     -S train.optimizer='bfsrs-medium' \
     -S program.prompting='structured' \
-    -S lm.model='llama-3-8b'
+    -S lm.model='llama-3.3-70b'
+
+dvc exp run --queue \
+    -S task='erx' \
+    -S train.dataset.path='bdsaglam/web_nlg-erx-concat' \
+    -S train.dataset.name='release_v3.0_en' \
+    -S train.dataset.split='"train[:100]"' \
+    -S evaluation.dataset.path='bdsaglam/web_nlg-erx-concat' \
+    -S evaluation.dataset.name='release_v3.0_en' \
+    -S evaluation.dataset.split='"dev"' \
+    -S train.ensemble='no' \
+    -S lm.temperature='0.0' \
+    -S run='1' \
+    -S train.optimizer='miprov2-medium' \
+    -S program.prompting='structured' \
+    -S lm.model='llama-3.3-70b'
+
+dvc exp run --queue \
+    -S task='erx' \
+    -S train.dataset.path='bdsaglam/web_nlg-erx-concat' \
+    -S train.dataset.name='release_v3.0_en' \
+    -S train.dataset.split='"train[:100]"' \
+    -S evaluation.dataset.path='bdsaglam/web_nlg-erx-concat' \
+    -S evaluation.dataset.name='release_v3.0_en' \
+    -S evaluation.dataset.split='"dev"' \
+    -S train.ensemble='no' \
+    -S lm.temperature='0.0' \
+    -S run='1' \
+    -S train.optimizer='noop' \
+    -S program.prompting='structured' \
+    -S lm.model='llama-3.3-70b'
+
+dvc exp run --queue \
+    -S task='erx' \
+    -S train.dataset.path='bdsaglam/web_nlg-erx-concat' \
+    -S train.dataset.name='release_v3.0_en' \
+    -S train.dataset.split='"train[:100]"' \
+    -S evaluation.dataset.path='bdsaglam/web_nlg-erx-concat' \
+    -S evaluation.dataset.name='release_v3.0_en' \
+    -S evaluation.dataset.split='"dev"' \
+    -S train.ensemble='no' \
+    -S lm.temperature='0.0' \
+    -S run='1' \
+    -S train.optimizer='miprov2-light' \
+    -S program.prompting='structured' \
+    -S lm.model='llama-3.3-70b'
 
